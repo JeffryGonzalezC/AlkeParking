07575b05f55119db5a4e9a81902a43ecd529f4ec
cristian-meli <cristian.cuevas@mercadolibre.cl>
2022-05-26 14:17:23 -0400

    definidos propiedades y funciones de clase ParkingSpace

--- app/src/main/java/com/example/alkeparking/Proyect/ParkingSpace.kt
+++ app/src/main/java/com/example/alkeparking/Proyect/ParkingSpace.kt
@@ -1,3 +1,33 @@
 package com.example.alkeparking.Proyect
 
-data class ParkingSpace(var vehicle: Vehicle)
+import java.util.*
+const val MINUTES_IN_MILLISECONDS = 6000
+
+
+data class ParkingSpace(
+    var vehicle: Vehicle,
+    val checkInTime: Calendar,
+    val discountCard: String?,
+    val parking: Parking
+) {
+    val parkedTime: Long
+        get() {
+            return (Calendar.getInstance().timeInMillis - checkInTime.timeInMillis) / MINUTES_IN_MILLISECONDS
+        }
+
+    fun checkOutVehicle(plate: String) {
+        for (vehicle in parking.vehicles) {
+            if (vehicle.plate == plate) {
+                onSuccess(0)
+            } else onError()
+        }
+    }
+
+    fun onSuccess(total: Int) {
+
+    }
+
+    fun onError() {
+
+    }
+}
