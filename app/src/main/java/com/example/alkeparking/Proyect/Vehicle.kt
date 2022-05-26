ba0c3d7ba44bb684f3694cfe80e7e1d4f8b1f56a
JeffryGonzalezC <jeffry.gonzalez@mercadolibre.com.co>
2022-05-26 15:15:53 -0500

    Se agregan parametros a la clase vehicle

--- app/src/main/java/com/example/alkeparking/Proyect/Vehicle.kt
+++ app/src/main/java/com/example/alkeparking/Proyect/Vehicle.kt
@@ -1,6 +1,11 @@
 package com.example.alkeparking.Proyect
 
-data class Vehicle(val plate: String){
+import java.util.*
+
+data class Vehicle(val plate: String,val type: VehicleType,
+                   val checkInTime: Calendar,
+                   val discountCard: String? = null){
+
 
     //Function state that two Vehicles are equal if their plates are equal
     override fun equals(other: Any?): Boolean {
@@ -14,4 +19,8 @@ data class Vehicle(val plate: String){
     //Function state that the hashCode (Used internally in search functions
     //in sets and arrays) is the hashCode of the plate
     override fun hashCode(): Int = this.plate.hashCode()
+
+    enum class VehicleType(val fee: Int) {
+        AUTO(20), MOTO(15), MINIBUS(25), BUS(30)
+    }
 }
